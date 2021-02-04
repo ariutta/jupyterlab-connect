29c5f06a26f0dea2fbbb70a43b3b88d91ac1e1ee
Anders Riutta <git@andersriutta.com>
2021-02-03 16:34:45 -0800

    Automatically assign ports. If jupyter server already running, connect to it.

--- lib/connect.sh
+++ lib/connect.sh
@@ -3,36 +3,37 @@
 OUTPUT_FILE=$(mktemp) || exit 1
 
 TARGET_DIR="$(readlink -f "$1")"
-port="$2"
 
-# TODO: DRY this up. There's duplicated for code in here, e.g., for getting the
-# token for a jupyter server running on a given port.
+jq_output_cmd='.port, .token'
 
-# Note: --arg always makes strings, so we need to convert '.port' (in the JSON) from number
-# to string in order to match
-
-if ! direnv exec "$TARGET_DIR" jupyter notebook list >/dev/null 2>&1; then
+if ! direnv exec "$TARGET_DIR" jupyter server list >/dev/null 2>&1; then
   # If we can't run this command, tell the user. Example: direnv not allowed.
-  echo "Failed to get list of running notebooks" >/dev/stderr
-  direnv exec "$TARGET_DIR" jupyter notebook list >/dev/stderr 1>&2
-elif direnv exec "$TARGET_DIR" jupyter notebook list --jsonlist 2>/dev/null |
-  jq -e --arg port "$port" 'map(select((.port | tostring) == $port)) | length > 0' \
-    >/dev/null; then
+  echo "Failed to get list of running servers" 1>&2
+  # the following may replicate the error so the user can see it:
+  direnv exec "$TARGET_DIR" jupyter server list 1>&2
+elif direnv exec "$TARGET_DIR" jupyter server list --jsonlist 2>/dev/null |
+  jq -e 'length > 0' >/dev/null; then
 
-  notebook_dir="$(direnv exec "$TARGET_DIR" jupyter notebook list --jsonlist 2>/dev/null |
-    jq -r --arg port "$port" 'map(select((.port | tostring) == $port)) | first | .notebook_dir')"
+  echo "Jupyter server(s) already running" 1>&2
+  echo "* for $TARGET_DIR" 1>&2
 
-  if [[ "$(readlink -f "$notebook_dir")" == "$TARGET_DIR" ]]; then
-    echo "Jupyter server on Port $port already running." >/dev/stderr
-    token=$(direnv exec "$TARGET_DIR" jupyter notebook list --jsonlist 2>/dev/null |
-      jq -r --arg port "$port" 'map(select((.port | tostring) == $port)) | first | .token')
-    echo "$token"
-  else
-    echo "Port $port already in use. Please specify an unused port and try again." >/dev/stderr
+  jupyter_connection_details="$(direnv exec "$TARGET_DIR" jupyter server list --jsonlist 2>/dev/null | jq -r 'first')"
+
+  port=$(echo "$jupyter_connection_details" | jq -r '.port')
+  root_dir=$(echo "$jupyter_connection_details" | jq -r '.root_dir')
+
+  normalized_root_dir="$(readlink -f "$root_dir")"
+  normalized_target_dir="$(readlink -f "$TARGET_DIR")"
+
+  if [[ "$normalized_root_dir" != "$normalized_target_dir" ]]; then
+    echo "Normalized root_dir must match normalized TARGET_DIR, but '$normalized_root_dir' != '$normalized_target_dir'" 1>&2
+    exit 1
   fi
+
+  echo "* connecting to Jupyter server on port $port..." 1>&2
+  echo "$jupyter_connection_details" | jq -r "$jq_output_cmd"
 else
-  BASE_SERVER_START_CMD="jupyter lab --no-browser --port=$port"
-  server_start_cmd=""
+  BASE_SERVER_START_CMD="jupyter lab --no-browser"
 
   # Limiting memory available to the process.
   # Doing this will avoid OOM errors that can crash the system.
@@ -46,17 +47,30 @@ else
     server_start_cmd="$BASE_SERVER_START_CMD"
   fi
 
-  # NOTE: We need to wait until jupyter is running before trying to get the token.
-  # Previously, we watched for the OUTPUT_FILE to contain a URL.
+  # NOTE: We wait until jupyter is running before we try getting the token.
+
+  #############################################################################
+  # Method #1 (deprecated): watch for the OUTPUT_FILE to contain a URL
+  #############################################################################
+
+  # An old commit where we were using this option:
   # https://github.com/ariutta/jupyterlab-connect/blob/5c67493f5c6e0920f554176c37e577395c998fa1/jupyterlab-launch#L124
+
+  # Using perl:
   # watch -t -g '[[ -f '"$OUTPUT_FILE"' ]] && perl -ne "print if s/(^|.*?[ \"])(http.*?)([\" >].*|$)/\$2/"' "$OUTPUT_FILE"
-  # an alternative way of doing the same thing:
+
+  # Using grep (an alternative way of doing the same thing):
   # watch -t -g "[[ -f "$OUTPUT_FILE" ]] && grep -E '(^|.*?[ \"])(http.*?)([\" >].*|$)'" "$OUTPUT_FILE"
 
-  token=$( (nohup direnv exec "$TARGET_DIR" sh -c "$server_start_cmd" >"$OUTPUT_FILE" 2>&1 &) &&
-    watch -t -g 'direnv exec '"$TARGET_DIR"' jupyter notebook list' "$OUTPUT_FILE" >/dev/null &&
-    direnv exec "$TARGET_DIR" jupyter notebook list --jsonlist 2>/dev/null |
-    jq -r --arg port "$port" 'map(select((.port | tostring) == $port)) | first | .token')
+  #############################################################################
+  # Method #2 (preferred): watch for 'jupyter server list' to show jupyter server running.
+  #############################################################################
 
-  echo "$token"
+  (nohup direnv exec "$TARGET_DIR" sh -c "$server_start_cmd" >"$OUTPUT_FILE" 2>&1 &) &&
+    watch -t -g 'direnv exec '"$TARGET_DIR"' jupyter server list' "$OUTPUT_FILE" >/dev/null &&
+    direnv exec "$TARGET_DIR" jupyter server list --jsonlist 2>/dev/null |
+    jq -r "first | $jq_output_cmd"
+
+  # TODO: if the server ever finishes starting before the watch starts, the
+  #       watch will never end, meaning the connection process will hang.
 fi
--- lib/main.sh
+++ lib/main.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 
 # Created by argbash-init v2.8.1
-# ARG_OPTIONAL_SINGLE([port],[p],[Port of Jupyter server],[8889])
 # ARG_OPTIONAL_BOOLEAN([browser],[],[Open the notebook in a browser after startup.],[on])
 # ARG_OPTIONAL_REPEATED([tunnel],[t],[<port on Jupyter server>:<remote server address>:<port on remote server>\n  Create an SSH tunnel. Can be specified multiple times to create multiple tunnels.\n  Example: Make a remote PostgreSQL server accessible to your Jupyter server:\n             -t 3333:database.example.org:5432\n],[])
 # ARG_POSITIONAL_DOUBLEDASH([])
@@ -22,19 +21,19 @@ cleanup_complete=0
 
 cleanup() {
   if [[ $connection_attempted -eq 1 ]]; then
-    echo "******************************************"
+    echo "******************************************" 1>&2
     cleanup_msg="Disconnecting from Jupyter server"
     if [ "$SERVER_IS_REMOTE" ]; then
-      echo "$cleanup_msg (remote)"
+      echo "$cleanup_msg (remote)" 1>&2
     else
-      echo "$cleanup_msg (local)"
+      echo "$cleanup_msg (local)" 1>&2
     fi
-    echo "******************************************"
+    echo "******************************************" 1>&2
 
     if [ "$SERVER_IS_REMOTE" ]; then
       if ssh -qS "$ssh_control_path" -O check "$JUPYTER_SERVER_ADDRESS" 2>/dev/null; then
         ssh -S "$ssh_control_path" "$JUPYTER_SERVER_ADDRESS" 'bash -s' -- \
-          "$TARGET_DIR" "$port" <"$SCRIPT_DIR/jupyter-notebook-stop.sh"
+          "$TARGET_DIR" "$jupyter_server_port" <"$SCRIPT_DIR/jupyter-notebook-stop.sh"
 
         for tunnel in $tunnels; do
           ssh -S "$ssh_control_path" "$JUPYTER_SERVER_ADDRESS" 'bash -s' -- \
@@ -44,14 +43,14 @@ cleanup() {
         ssh -qS "$ssh_control_path" -O exit "$JUPYTER_SERVER_ADDRESS"
       fi
     else
-      sh "$SCRIPT_DIR/jupyter-notebook-stop.sh" "$TARGET_DIR" "$port"
+      sh "$SCRIPT_DIR/jupyter-notebook-stop.sh" "$TARGET_DIR" "$jupyter_server_port"
 
       for tunnel in $tunnels; do
         sh "$SCRIPT_DIR/close-tunnel.sh" "$tunnel" "$ssh_control_path_expr"
       done
     fi
 
-    echo "$PROGNAME: goodbye"
+    echo "$PROGNAME: goodbye" 1>&2
   fi
 
   cleanup_complete=1
@@ -106,10 +105,14 @@ fi
 ############################################
 # Make pretty names for args parsed by argbash
 target="$_arg_target"
-port="$_arg_port"
 tunnels="$_arg_tunnel"
 browser="$_arg_browser"
 ############################################
+
+get_free_port() {
+  # from https://unix.stackexchange.com/questions/55913/whats-the-easiest-way-to-find-an-unused-local-port#comment271991_132524
+  python -c 'import socket; s=socket.socket(); s.bind(("", 0)); print(s.getsockname()[1]); s.close()'
+}
 
 # ssh doesn't like '~' in the paths
 ssh_safe_target=$(echo "$target" | sed 's/:~/:$HOME/')
@@ -123,28 +126,16 @@ else
   TARGET_DIR="$ssh_safe_target"
 fi
 
-token=""
+jupyter_connection_details=""
 
 remote_hosts_controlled=()
 
-echo "******************************************"
 if [ $SERVER_IS_REMOTE ]; then
-  echo "Connecting to Jupyter server on $JUPYTER_SERVER_ADDRESS (remote)"
-  echo "for $ssh_safe_target"
-else
-  echo "Connecting to Jupyter server on $(hostname) (local)"
-  echo "for $ssh_safe_target"
-fi
-echo "******************************************"
+  echo "******************************************" 1>&2
+  echo "Connecting to remote Jupyter server ($JUPYTER_SERVER_ADDRESS)" 1>&2
+  echo "* root_dir: $TARGET_DIR" 1>&2
+  echo "******************************************" 1>&2
 
-if [[ -z "$JUPYTER_SERVER_ADDRESS" ]]; then
-  if direnv exec "$TARGET_DIR" jupyter-lab --version >/dev/null 2>&1; then
-    token=$(bash "$SCRIPT_DIR/connect.sh" "$TARGET_DIR" "$port")
-  else
-    error_exit "jupyter-lab not installed locally. Did you mean to run on a remote server?
-    Run $PROGNAME --help to see how to specify a remote server and target directory."
-  fi
-else
   # We only want to specify ControlMaster=yes the first time.
   if ssh -qS "$ssh_control_path" -O check "$JUPYTER_SERVER_ADDRESS" 2>/dev/null; then
     ControlMaster="no"
@@ -152,35 +143,70 @@ else
     ControlMaster="yes"
   fi
 
-  token=$(ssh -o ControlMaster="$ControlMaster" \
+  jupyter_connection_details=$(ssh -o ControlMaster="$ControlMaster" \
     -o ControlPersist=yes \
     -S "$ssh_control_path" \
     "$JUPYTER_SERVER_ADDRESS" 'bash -s' -- \
-    "$TARGET_DIR" "$port" <"$SCRIPT_DIR/connect.sh" ||
-    connection_attempted=1)
+    "$TARGET_DIR" <"$SCRIPT_DIR/connect.sh")
 
-  # TODO: could we use mosh for the tunnel?
-  # related: https://github.com/mobile-shell/mosh/issues/24#issuecomment-303151487
+  # TODO: should we use '|| connection_attempted=1'?
+#  jupyter_connection_details=$(ssh -o ControlMaster="$ControlMaster" \
+#    -o ControlPersist=yes \
+#    -S "$ssh_control_path" \
+#    "$JUPYTER_SERVER_ADDRESS" 'bash -s' -- \
+#    "$TARGET_DIR" <"$SCRIPT_DIR/connect.sh" ||
+#    connection_attempted=1)
 
   remote_hosts_controlled+=("$JUPYTER_SERVER_ADDRESS")
+else
+  echo "******************************************" 1>&2
+  echo "Connecting to local Jupyter server ($(hostname))" 1>&2
+  echo "* root_dir: $TARGET_DIR" 1>&2
+  echo "******************************************" 1>&2
 
-  echo ""
-  echo "Opening tunnel to allow browser to connect to Jupyter server:"
-  echo "localhost:$port on $(hostname) <-> $JUPYTER_SERVER_ADDRESS:$port"
-  ssh -S "$ssh_control_path" -L "$port":localhost:"$port" -N -f "$JUPYTER_SERVER_ADDRESS"
-  # -S: re-use existing ssh connection
-  # -L: local port forwarding
-  # -f: send to background
-  # -N: don't issue any commands on remote server
+  if direnv exec "$TARGET_DIR" jupyter-lab --version >/dev/null 2>&1; then
+    jupyter_connection_details=$(bash "$SCRIPT_DIR/connect.sh" "$TARGET_DIR")
+  else
+    error_exit "jupyter-lab not installed locally. Did you mean to run on a remote server?
+    Run $PROGNAME --help to see how to specify a remote server and target directory."
+  fi
 fi
 
 connection_attempted=1
 
-if [[ -z "$token" ]] || [[ "$token" == 'null' ]]; then
-  error_exit "No token found"
+if [ -z "$jupyter_connection_details" ] || [ "$jupyter_connection_details" == 'null' ]; then
+  error_exit "Unable to get jupyter_connection_details."
 fi
 
-url="http://localhost:$port/?token=$token"
+# NOTE: the following is kludgy, but we can't assume the user's computer has jq
+jupyter_connection_details_arr=()
+for value in $jupyter_connection_details;
+do
+  jupyter_connection_details_arr+=("$value")
+done
+
+jupyter_server_port=${jupyter_connection_details_arr[0]}
+token=${jupyter_connection_details_arr[1]}
+
+localhost_port=$(get_free_port)
+
+if [ ! -z "$JUPYTER_SERVER_ADDRESS" ]; then
+  # TODO: could we use mosh for the tunnel?
+  # related: https://github.com/mobile-shell/mosh/issues/24#issuecomment-303151487
+
+  echo "" 1>&2
+  echo "Opening tunnel to allow browser to connect to Jupyter server:" 1>&2
+  echo "  localhost:$localhost_port <-> $JUPYTER_SERVER_ADDRESS:$jupyter_server_port (localhost is $(hostname))" 1>&2
+  ssh -S "$ssh_control_path" -L "$localhost_port":localhost:"$jupyter_server_port" -N -f "$JUPYTER_SERVER_ADDRESS"
+  # -S: re-use existing ssh connection
+  # -L: local port forwarding
+  # -f: send to background
+  # -N: don't issue any commands on remote server
+
+  echo "  Success! Tunnel created." 1>&2
+fi
+
+url="http://localhost:$localhost_port/?token=$token"
 
 for tunnel in $tunnels; do
   if [ $SERVER_IS_REMOTE ]; then
@@ -191,7 +217,7 @@ for tunnel in $tunnels; do
   fi
 done
 
-echo ""
+echo "" 1>&2
 to_view_msg="To view the notebook, visit:\n   $url"
 if [ "$browser" == 'on' ]; then
   if xdg-open --version >/dev/null 2>&1; then
